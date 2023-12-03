06a28b61921956397b73dc246b9920d5d96540cc
Tyler694 <141347012+Tyler694@users.noreply.github.com>
2023-12-03 18:47:34 +0000

    map drew
    I created the map

--- index.js
+++ index.js
@@ -3,14 +3,53 @@ const c = canvas.getContext("2d")
 
 TILE_SIZE = 64
 
-canvas.width = 16 * TILE_SIZE
-canvas.height = 9 * TILE_SIZE
+canvas.width = 1024
+canvas.height = 576
+
+xOffset = 0
+yOffset = 0
+
+c.fillStyle = "black"
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+map = [
+    ["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","#","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","0","0","0","0","0","0","0","0","0","0","0","0","0","0","#"],
+    ["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"],
+]
+
+
+
 
 function mainLoop() {
-    drawCanvas()
+    drawmap()
 }
 
-function drawCanvas() {
-    c.fillStyle = "black"
-    c.fillRect(0,0,canvas.clientWidth,canvas.height)
+
+function drawmap() {
+    yOffset = 0
+    for(row of map) {
+        for(symbol of row) {
+            if (symbol == "#") {
+                    c.fillStyle = "grey"
+                    c.fillRect(xOffset*TILE_SIZE, yOffset, TILE_SIZE-1, TILE_SIZE-1)
+                    xOffset++
+                } else {
+                    c.fillStyle = "lightgrey"
+                    c.fillRect(xOffset*TILE_SIZE, yOffset, TILE_SIZE-1, TILE_SIZE-1)
+                    xOffset++
+                }
+        }
+        xOffset = 0
+        yOffset += TILE_SIZE
+    }
 }
+
+
+setInterval(mainLoop, 10)
